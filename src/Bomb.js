7d74778792e2b02330be7b321f848aa49d993b06
github username <github email address>
2018-10-30 14:46:28 -0400

    sliding state with react.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+
+  render(){
+    let text = '';
+    if (this.state.secondsLeft == 0)
+    {
+      text = 'Boom!'
+    }
+    else
+    {
+      text= `${this.state.secondsLeft} seconds left before I go boom!`
+    }
+
+    return (
+      <div>{text}</div>
+    )
+  }
+}
